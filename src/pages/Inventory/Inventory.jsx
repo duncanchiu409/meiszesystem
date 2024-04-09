a7c8c81877501cb036184bf17a7413be1bb398b2
duncanchiu409 <chiuduncan409@gmail.com>
2024-04-09 16:22:48 +0800

    day 06: update Inventory List

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,22 +1,101 @@
 import React, { useState, useEffect, useRef } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { DownloadTableExcel } from "react-export-table-to-excel";
 import { db } from "../../firebase";
 import { onValue, ref, remove } from "firebase/database";
 import { FaSearch } from "react-icons/fa";
 import "../../App.css";
 import { useTranslation } from "react-i18next";
+import { Box } from "@mui/material";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EditIcon from "@mui/icons-material/Edit";
 
 const Inventory = () => {
   const [search, setSearch] = useState("");
   const [inventory, setInventory] = useState([]);
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+  const navigate = useNavigate()
+
+  const columns = [
+    {
+      field: "status",
+      headerName: t("Inventory List.Status"),
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "refno",
+      headerName: t("Inventory List.Ref no"),
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "date",
+      headerName: t("Inventory List.Date"),
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "product",
+      headerName: t("Inventory List.Product"),
+      flex: 1,
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "quantity",
+      headerName: t("Inventory List.Quantity"),
+      type: "number",
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "cost",
+      headerName: t("Inventory List.Total Cost"),
+      type: "number",
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "remark",
+      headerName: t("Inventory List.Remark"),
+      type: "number",
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: t("Table Actions.actions"),
+      headerClassName: "custom-container-table-head",
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<VisibilityIcon />}
+          onClick={() => {
+            navigate(`view/${params.id}`);
+          }}
+          label={t("Table Actions.view")}
+          showInMenu
+        />,
+        <GridActionsCellItem
+          icon={<EditIcon />}
+          onClick={() => {
+            navigate(`update/${params.id}`);
+          }}
+          label={t("Table Actions.edit")}
+          showInMenu
+        />,
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          onClick={() => {}}
+          label={t("Table Actions.delete")}
+          showInMenu
+        />,
+      ],
+    },
+  ]
 
   useEffect(() => {
     const getInventory = () => {
       onValue(ref(db, "inventory"), (snapshot) => {
         if (snapshot.val() !== null) {
-          setInventory({ ...snapshot.val() });
+          const obj = snapshot.val()
+          setInventory(() => Object.keys(obj).map((key) => ({ id: key, ...obj[key] })));
         }
       });
     };
@@ -25,37 +104,56 @@ const Inventory = () => {
   }, []);
 
   const tableRef = useRef(null);
+
   return (
     <div className="main">
-      <div
-        className="App"
-        style={{ width: "100%", padding: "100px", height: "1000px" }}
-      >
-        <div style={{ padding: "0px 215px" }} className="container">
-          <div className="input-wrapper">
-            <FaSearch id="search-icon" />
-            <input
-              type="text"
-              className="inputField"
-              placeholder="Search Order No."
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
+      <div className="App">
+        <div className="container">
+          <div className="text-end">
+            <h1>{t("table.Inventory List")}</h1>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <div className="input-wrapper">
+                <FaSearch id="search-icon" />
+                <input
+                  type="text"
+                  className="inputField"
+                  placeholder="Search Order No."
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
+                />
+              </div>
+              <NavLink to="add" className="btn-create">
+                {t("Excel.Create")}
+              </NavLink>
+              <DownloadTableExcel
+                filename="Inventory table"
+                sheet="Inventory"
+                currentTableRef={tableRef.current}
+              >
+                <button className="btn-create">
+                  {" "}
+                  {t("Excel.Export Excel")}{" "}
+                </button>
+              </DownloadTableExcel>
+            </div>
           </div>
 
-          <div className="text-end">
-            <h1>{t('table.Inventory List')}</h1>
-            <NavLink to="add" className="btn-create">
-              {t('Excel.Create')}
-            </NavLink>
-            <DownloadTableExcel
-              filename="Inventory table"
-              sheet="Inventory"
-              currentTableRef={tableRef.current}
-            >
-              <button className="btn-create"> {t('Excel.Export Excel')} </button>
-            </DownloadTableExcel>
+          {/* div wtih .custom-container to override the bootstrap css */}
+          <div className="custom-container">
+            <Box sx={{ mt: 1, color: "white" }}>
+              <DataGrid
+                autoHeight
+                sx={{ minHeight: 400, color: "var(--sidebar-font-color)" }}
+                rows={inventory}
+                columns={columns}
+                initialState={{
+                  pagination: { paginationModel: { page: 0, pageSize: 5 } },
+                }}
+                pageSizeOptions={[5, 10]}
+              />
+            </Box>
           </div>
+
           <table className="styled-table" ref={tableRef}>
             <thead>
               <tr>
--- src/pages/Inventory/StockDetail.jsx
+++ src/pages/Inventory/StockDetail.jsx
@@ -1,102 +1,143 @@
 import React, { useState, useEffect, useRef } from "react";
-import{NavLink} from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom";
 import { DownloadTableExcel } from "react-export-table-to-excel";
-import {db} from "../../firebase";
-import {onValue, ref} from 'firebase/database'
+import { db } from "../../firebase";
+import { onValue, ref } from "firebase/database";
 import { FaSearch } from "react-icons/fa";
 import "../../App.css";
 import { useTranslation } from "react-i18next";
+import { Box } from "@mui/material";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EditIcon from "@mui/icons-material/Edit";
 
 const StockDetail = () => {
-    const [search, setSearch] = useState("");
-    const [StockDetail, setStockDetail] = useState([]);
-    const { t } = useTranslation()
-  
-    useEffect(() => {
-      const getStockDetail = () => {
-        onValue(ref(db, "StockDetail"), (snapshot) => {
-          if (snapshot.val() !== null) {
-            setStockDetail({ ...snapshot.val() });
-          }
-        });
-      };
-  
-      getStockDetail();
-    }, []);
-  
-    const tableRef = useRef(null);
-    return (
-      <div className="main">
-        <div
-          className="App"
-          style={{ width: "100%", padding: "100px", height: "1000px" }}
-        >
-          <div style={{ padding: "0px 215px" }} className="container">
-            <div className="input-wrapper">
-              <FaSearch id="search-icon" />
-              <input
-                type="text"
-                className="inputField"
-                placeholder="Search Product"
-                onChange={(e) => setSearch(e.target.value)}
-                value={search}
-              />
-            </div>
-  
-            <div className="text-end">
-              <h1>{t('table.Stock Detail')}</h1>
+  const [search, setSearch] = useState("");
+  const [StockDetail, setStockDetail] = useState([]);
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const columns = [
+    {
+      field: "product",
+      flex: 0.3,
+      headerName: t("Purchase Order List.Purchase Order no"),
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "date",
+      flex: 0.3,
+      headerName: t("Purchase Order List.Date"),
+      headerClassName: "custom-container-table-head",
+    },
+    {
+      field: "supplier",
+      flex: 0.3,
+      headerName: t("Purchase Order List.Supplier"),
+      headerClassName: "custom-container-table-head",
+    },
+  ]
+
+  useEffect(() => {
+    const getStockDetail = () => {
+      onValue(ref(db, "StockDetail"), (snapshot) => {
+        if (snapshot.val() !== null) {
+          const obj = snapshot.val()
+          // setStockDetail(() => Object.keys(obj).map((key) => ({})));
+          console.log(obj)
+        }
+      });
+    };
+
+    getStockDetail();
+  }, []);
+
+  const tableRef = useRef(null);
+  return (
+    <div className="main">
+      <div className="App">
+        <div className="container">
+          <div className="text-end">
+            <h1>{t("table.Stock Detail")}</h1>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <div className="input-wrapper">
+                <FaSearch id="search-icon" />
+                <input
+                  type="text"
+                  className="inputField"
+                  placeholder="Search Product"
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
+                />
+              </div>
               <NavLink to="add" className="btn-create">
-                {t('Excel.Create')}
+                {t("Excel.Create")}
               </NavLink>
               <DownloadTableExcel
                 filename="Stock Detail table"
                 sheet="Stock Detail"
                 currentTableRef={tableRef.current}
               >
-                <button className="btn-create"> {t('Excel.Export Excel')} </button>
+                <button className="btn-create">
+                  {" "}
+                  {t("Excel.Export Excel")}{" "}
+                </button>
               </DownloadTableExcel>
             </div>
-            <table className="styled-table" ref={tableRef}>
-              <thead>
-                <tr>
-
-                  <th style={{ textAlign: "center" }}>Product</th>
-                  <th style={{ textAlign: "center" }}>Quantity</th>
-                  <th style={{ textAlign: "center" }}>WareHouse</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.keys(StockDetail)
-                  .filter((product) => {
-                    return search.toLowerCase() === ""
-                      ? product
-                      : product.toLowerCase().includes(search);
-                  })
-                  .sort((a, b) =>
-                    StockDetail[a].quantity > StockDetail[b].quantity ? 1 : -1
-                  )
-                  .map((product) => {
-                    return (
-                      <tr>
-                        <td style={{ textAlign: "center" }}>
-                          {product}
-                        </td>
-                        <td style={{ textAlign: "center" }}>
-                          {StockDetail[product].quantity}
-                        </td>
-                        <td style={{ textAlign: "center" }}>
-                            Tuen Mun
-                        </td>
-                        
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
           </div>
+
+          {/* div wtih .custom-container to override the bootstrap css */}
+          <div className="custom-container">
+            <Box sx={{ mt: 1, color: "white" }}>
+              <DataGrid
+                autoHeight
+                sx={{ minHeight: 400, color: "var(--sidebar-font-color)" }}
+                rows={StockDetail}
+                columns={columns}
+                initialState={{
+                  pagination: { paginationModel: { page: 0, pageSize: 5 } },
+                }}
+                pageSizeOptions={[5, 10]}
+              />
+            </Box>
+          </div>
+
+          <table className="styled-table" ref={tableRef} style={{ display: 'none' }}>
+            <thead>
+              <tr>
+                <th style={{ textAlign: "center" }}>Product</th>
+                <th style={{ textAlign: "center" }}>Quantity</th>
+                <th style={{ textAlign: "center" }}>WareHouse</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(StockDetail)
+                .filter((product) => {
+                  return search.toLowerCase() === ""
+                    ? product
+                    : product.toLowerCase().includes(search);
+                })
+                .sort((a, b) =>
+                  StockDetail[a].quantity > StockDetail[b].quantity ? 1 : -1
+                )
+                .map((product) => {
+                  return (
+                    <tr>
+                      <td style={{ textAlign: "center" }}>{product}</td>
+                      <td style={{ textAlign: "center" }}>
+                        {StockDetail[product].quantity}
+                      </td>
+                      <td style={{ textAlign: "center" }}>Tuen Mun</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default StockDetail
+export default StockDetail;
